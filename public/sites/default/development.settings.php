68bac55b4c225c244847ca901b0e8a0a4b5df221
tuutti <tuutti@iki.fi>
2021-10-26 05:27:51 +0300

    Env prefixes

---
+++ public/sites/default/development.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'dev-kasko-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'dev-kasko-en',
+  'fi' => 'dev-kasko-fi',
+  'sv' => 'dev-kasko-sv',
+  'ru' => 'dev-kasko-ru',
+];
---
+++ public/sites/default/staging.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'staging-kasko-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'staging-kasko-en',
+  'fi' => 'staging-kasko-fi',
+  'sv' => 'staging-kasko-sv',
+  'ru' => 'staging-kasko-ru',
+];
