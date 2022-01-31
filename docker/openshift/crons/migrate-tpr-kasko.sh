79fea354451ab9618ca59d2159489b3a155f8c06
Tapio Pietinsalo <tapio.pietinsalo@siili.com>
2022-01-31 09:56:46 +0200

    Revert "UHF-4113: Refactor shell script functions to separate file"
    This reverts commit 44417438249b3410d54a26faa5791a2e50f0f740.

--- docker/openshift/crons/migrate-tpr-kasko.sh
+++ docker/openshift/crons/migrate-tpr-kasko.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-source /crons/migrate.sh
-
 echo "Running Kasko TPR Migrations: $(date)"
+
+function populate_variables {
+  # Generate variables used to control which migrates needs
+  # to be reset and which ones needs to be skipped based on
+  # migrate status
+  MIGRATE_STATUS=$(drush migrate:status --format=json)
+  php ./docker/openshift/crons/migrate-status.php \
+    tpr_ontology_word_details \
+    "$MIGRATE_STATUS" > /tmp/migrate-tpr-kasko-source.sh \
+    $1
+
+  # Contains variables:
+  # - $RESET_STATUS
+  # - $SKIP_MIGRATE
+  # Both contains a space separated list of migrates
+  source /tmp/migrate-tpr-kasko-source.sh
+}
+
+function reset_status {
+  # Reset status of stuck migrations.
+  for ID in $RESET_STATUS; do
+    drush migrate:reset-status $ID
+  done
+}
+
+function run_migrate {
+  for ID in $SKIP_MIGRATE; do
+    if [ "$ID" == "$1" ]; then
+      return 1
+    fi
+  done
+  return 0
+}
 
 # Populate variables for the first run after deploy and
 # default migrate interval to 12 hours.
-populate_variables 43200 "/tmp/migrate-tpr-kasko-source.sh"
+populate_variables 43200
 
 while true
 do
@@ -19,7 +50,7 @@ do
   fi
   # Reset migrate status if migrate has been running for more
   # than 12 hours.
-  populate_variables 43200 "/tmp/migrate-tpr-kasko-source.sh"
+  populate_variables 43200
   # Never skip migrate after first time.
   SKIP_MIGRATE=
   # Sleep for 12 hours.
