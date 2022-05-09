2d33a95a1ee04852769f64148732822425258bbd
Lauri Kolehmainen <lauri.kolehmainen@siili.com>
2022-05-09 11:23:21 +0300

    UHF-5473: fixed override

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -5,8 +5,6 @@
  * Contains site specific overrides.
  */
 
-$databases['default']['default'] = [
-  'init_commands' => [
-    'sql_mode' => 'SET sql_mode="STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_ENGINE_SUBSTITUTION"',
-  ],
+$databases['default']['default']['init_commands'] = [
+  'sql_mode' => 'SET sql_mode="STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_ENGINE_SUBSTITUTION"',
 ];
