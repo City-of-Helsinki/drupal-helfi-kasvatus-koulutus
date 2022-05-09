26de114f654a3e91c396ebaf9729a8dc7a6deae8
Lauri Kolehmainen <lauri.kolehmainen@siili.com>
2022-05-09 11:11:46 +0300

    UHF-5473: modified sql_mode to support required GROUP BY operation

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -4,3 +4,9 @@
  * @file
  * Contains site specific overrides.
  */
+
+$databases['default']['default'] = [
+  'init_commands' => [
+    'sql_mode' => 'SET sql_mode="STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_ENGINE_SUBSTITUTION"',
+  ],
+];
