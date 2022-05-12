cc96c155fa0a22427c03623ef3af095808af00b0
tuutti <tuutti@iki.fi>
2022-05-12 07:29:40 +0300

    UHF-5074: Always redirect to www.hel.fi

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -4,3 +4,10 @@
  * @file
  * Contains site specific overrides.
  */
+
+if ($drush_options_uri = getenv('DRUSH_OPTIONS_URI')) {
+  if (str_contains($drush_options_uri, 'www.hel.fi')) {
+    $config['helfi_proxy.settings']['default_proxy_domain'] = 'www.hel.fi';
+  }
+}
+
