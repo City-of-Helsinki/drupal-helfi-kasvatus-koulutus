8796c09daa421054c7588eb40593d4135307ba72
Lauri Kolehmainen <lauri.kolehmainen@siili.com>
2022-11-24 13:34:32 +0200

    UHF-6917: added config to all.settings.php

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -14,3 +14,18 @@ if ($drush_options_uri = getenv('DRUSH_OPTIONS_URI')) {
     $config['helfi_proxy.settings']['default_proxy_domain'] = 'www.hel.fi';
   }
 }
+
+// Elasticsearch settings.
+if (getenv('ELASTICSEARCH_URL')) {
+  $config['elasticsearch_connector.cluster.elastic_kasko']['url'] = getenv('ELASTICSEARCH_URL');
+
+  if (getenv('ELASTIC_USER') && getenv('ELASTIC_PASSWORD')) {
+    $config['elasticsearch_connector.cluster.elastic_kasko']['options']['use_authentication'] = '1';
+    $config['elasticsearch_connector.cluster.elastic_kasko']['options']['authentication_type'] = 'Basic';
+    $config['elasticsearch_connector.cluster.elastic_kasko']['options']['username'] = getenv('ELASTIC_USER');
+    $config['elasticsearch_connector.cluster.elastic_kasko']['options']['password'] = getenv('ELASTIC_PASSWORD');
+  }
+}
+
+// Elastic proxy URL.
+$config['elastic_proxy.settings']['elastic_proxy_url'] = getenv('ELASTIC_PROXY_URL');
