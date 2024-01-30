267f7286641413e81dae8dfdbabb8ba415a1129e
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-01-30 12:10:46 +0200

    UHF-9531: Remove tunnistamo_return_url override

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,6 +1,5 @@
 <?php
 
-$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/test-kasvatus-ja-koulutus/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'test-kasko-assets';
 $config['helfi_proxy.settings']['prefixes'] = [
   'en' => 'test-childhood-and-education',
