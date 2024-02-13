1e91bb52db9cd35bade823b843efb17b35f89d16
tuutti <tuutti@iki.fi>
2024-02-13 11:03:59 +0200

    UHF-9546: Fixes

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,2 +1,11 @@
 <?php
 
+$config['helfi_proxy.settings']['asset_path'] = 'kasko-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'childhood-and-education',
+  'fi' => 'kasvatus-ja-koulutus',
+  'sv' => 'fostran-och-utbildning',
+  'ru' => 'childhood-and-education',
+  'zxx' => 'childhood-and-education',
+];
+
