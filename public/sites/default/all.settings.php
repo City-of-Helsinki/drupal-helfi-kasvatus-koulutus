8646178c0f6d7b07e2c81c366ba92444c99c69a7
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-12-02 12:40:55 +0200

    UHF-10549: Move elasticsearch settings to settings.php

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -9,17 +9,6 @@ $databases['default']['default']['init_commands'] = [
   'sql_mode' => 'SET sql_mode="STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_ENGINE_SUBSTITUTION"',
 ];
 
-// Elasticsearch settings.
-if (getenv('ELASTICSEARCH_URL')) {
-  $config['search_api.server.elastic_kasko']['backend_config']['connector_config']['url'] = getenv('ELASTICSEARCH_URL');
-
-  if (getenv('ELASTIC_USER') && getenv('ELASTIC_PASSWORD')) {
-    $config['search_api.server.elastic_kasko']['backend_config']['connector'] = 'basicauth';
-    $config['search_api.server.elastic_kasko']['backend_config']['connector_config']['username'] = getenv('ELASTIC_USER');
-    $config['search_api.server.elastic_kasko']['backend_config']['connector_config']['password'] = getenv('ELASTIC_PASSWORD');
-  }
-}
-
 // Elastic proxy URL.
 $config['elastic_proxy.settings']['elastic_proxy_url'] = getenv('ELASTIC_PROXY_URL');
 
