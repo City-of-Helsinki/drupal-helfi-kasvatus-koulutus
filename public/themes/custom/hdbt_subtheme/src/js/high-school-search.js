85719e943c0c9c3716e5ca63cb2850fab2644d1c
Tero Elonen <tero.elonen@druid.fi>
2025-02-12 10:22:14 +0200

    Fix javascript to follow es6 and ecma 2022 standards

--- public/themes/custom/hdbt_subtheme/src/js/high-school-search.js
+++ public/themes/custom/hdbt_subtheme/src/js/high-school-search.js
@@ -1,64 +1,52 @@
-(function(Drupal) {
-  Drupal.behaviors.HighSchoolSearch = {};
+((Drupal) => {
+  Drupal.behaviors.HighSchoolSearch = {
+    attach: function attach(context) {
+      // Set attributes based on whether the select should be disabled or not.
+      const toggleSelectActivity = (select, disabled) => {
+        const helpText = select.nextElementSibling;
+        if (disabled) {
+          helpText.textContent = Drupal.t(
+            'Filter on the form has been selected and some of the other ' +
+            'filters might be dimmed because of your selection. To use the ' +
+            'filters that were dimmed, select the option "All" on the selected filter.'
+          );
+          select.parentElement?.classList.add('hdbt--select-wrapper--disabled');
+          select.disabled = true;
+          select.dataset.ariaDisabled = 'true';
+        } else {
+          helpText.textContent = '';
+          select.parentElement?.classList.remove('hdbt--select-wrapper--disabled');
+          select.disabled = false;
+          select.dataset.ariaDisabled = 'false';
+        }
+      };
 
-  Drupal.behaviors.HighSchoolSearch.attach = function(context) {
-    // Find all select elements inside high school search.
-    const selectElements = context.querySelectorAll(
-      '.unit-search--high-school .js-form-item-emphasis .form-select, ' +
-      '.unit-search--high-school .js-form-item-mission .form-select'
-    );
+      // Disable all other selects except the one that has some OTHER option selected
+      // than 'All' and enable them all if they all have 'All' option selected.
+      const disableOtherSelects = (selected, selectElements) => {
+        if (selected.value !== 'All') {
+          selectElements.forEach((select) => toggleSelectActivity(select, select !== selected));
+        } else {
+          selectElements.forEach((select) => {
+            if (select !== selected) {
+              toggleSelectActivity(select, false);
+            }
+          });
+        }
+      };
 
-    selectElements.forEach(
-      function(select) {
-        disableOtherSelects(select, selectElements);
-      }
-    );
+      // Find all select elements inside high school search.
+      const selectElements = context.querySelectorAll(
+        '.unit-search--high-school .js-form-item-emphasis .form-select, ' +
+        '.unit-search--high-school .js-form-item-mission .form-select'
+      );
 
-    for (const select of selectElements) {
-      select.addEventListener('change', function(){
-        disableOtherSelects(this, selectElements);
+      selectElements.forEach((select) => disableOtherSelects(select, selectElements));
+
+      selectElements.forEach((select) => {
+        select.addEventListener('change', () => disableOtherSelects(select, selectElements));
       });
     }
   };
-
-  // Set attributes based on if the select should be disabled or not.
-  function toggleSelectActivity(select, disabled) {
-    const helpText = select.nextElementSibling;
-    if (disabled) {
-      helpText.textContent = Drupal.t(
-        'Filter on the form has been selected and some of the other ' +
-        'filters might be dimmed because of your selection. To use the ' +
-        'filters that were dimmed select the option All on the selected ' +
-        'filter.'
-      );
-      select.parentElement.classList.add('hdbt--select-wrapper--disabled');
-      select.disabled = true;
-      select.setAttribute('aria-disabled', 'true');
-    } else {
-      helpText.textContent = '';
-      select.parentElement.classList.remove(
-        'hdbt--select-wrapper--disabled'
-      );
-      select.disabled = false;
-      select.setAttribute('aria-disabled', 'false');
-    }
-  }
-
-  // Disable all other selects except the one that has some OTHER option selected
-  // than 'All' and enable them all if they all have 'All' option selected.
-  function disableOtherSelects(selected, selectElements) {
-    if (selected.value !== 'All') {
-      for (const select of selectElements) {
-        toggleSelectActivity(select, select !== selected);
-      }
-    }
-    else {
-      for (const select of selectElements) {
-        if (select !== selected) {
-          toggleSelectActivity(select, false);
-        }
-      }
-    }
-  }
 
 })(Drupal);
