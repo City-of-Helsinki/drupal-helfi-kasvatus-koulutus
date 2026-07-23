baf2d446c9ec252424df106dfbe43ad54f87495a
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-07-23 15:18:15 +0300

    Fixed permission names.

--- public/modules/custom/helfi_kasko_content/tests/src/Kernel/PermissionsHooksTest.php
+++ public/modules/custom/helfi_kasko_content/tests/src/Kernel/PermissionsHooksTest.php
@@ -27,13 +27,62 @@ class PermissionsHooksTest extends KernelTestBase {
   ];
 
   /**
-   * Test that the school editor permissions are granted.
+   * Test that the role permissions are granted.
    */
   public function testGrantPermissions(): void {
     $hook = new PermissionsHooks();
     $permissions = $hook->grantPermissions();
 
     $this->assertEquals([
+      'admin' => [
+        'create comprehensive_school_subpage content',
+        'delete any comprehensive_school_subpage content',
+        'delete own comprehensive_school_subpage content',
+        'delete comprehensive_school_subpage revisions',
+        'edit any comprehensive_school_subpage content',
+        'edit own comprehensive_school_subpage content',
+        'revert comprehensive_school_subpage revisions',
+        'set comprehensive_school_subpage published on date',
+        'translate comprehensive_school_subpage node',
+        'view comprehensive_school_subpage revisions',
+      ],
+      'comprehensive_school_editor' => [
+        'create comprehensive_school_subpage content',
+        'delete own comprehensive_school_subpage content',
+        'edit any comprehensive_school_subpage content',
+        'edit own comprehensive_school_subpage content',
+        'revert comprehensive_school_subpage revisions',
+        'set comprehensive_school_subpage published on date',
+        'translate comprehensive_school_subpage node',
+        'view any unpublished comprehensive_school_subpage content',
+        'view comprehensive_school_subpage revisions',
+      ],
+      'content_producer' => [
+        'create comprehensive_school_subpage content',
+        'delete own comprehensive_school_subpage content',
+        'edit any comprehensive_school_subpage content',
+        'edit own comprehensive_school_subpage content',
+        'revert comprehensive_school_subpage revisions',
+        'set comprehensive_school_subpage published on date',
+        'translate comprehensive_school_subpage node',
+        'view any unpublished comprehensive_school_subpage content',
+        'view comprehensive_school_subpage revisions',
+      ],
+      'editor' => [
+        'create comprehensive_school_subpage content',
+        'delete any comprehensive_school_subpage content',
+        'delete own comprehensive_school_subpage content',
+        'delete comprehensive_school_subpage revisions',
+        'edit any comprehensive_school_subpage content',
+        'edit own comprehensive_school_subpage content',
+        'revert comprehensive_school_subpage revisions',
+        'set comprehensive_school_subpage published on date',
+        'translate comprehensive_school_subpage node',
+        'view comprehensive_school_subpage revisions',
+      ],
+      'read_only' => [
+        'view any unpublished comprehensive_school_subpage content',
+      ],
       'school_editor' => [
         'create announcement content',
         'delete own announcement content',
